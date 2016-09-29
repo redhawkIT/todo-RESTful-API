19e2018e85e2e5ef8501adffe1994f7d67e9c29e
redhawkIT <khodkodmitry10011983@hotmail.com>
2016-09-29 14:17:20 -0500

    setup

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express') // call express
+const app = express() // define our app using express
+const bodyParser = require('body-parser')
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+const port = process.env.PORT || 8080 // set our port
+
+// ROUTES FOR OUR API
+// =============================================================================
+const router = express.Router() // get an instance of the express Router
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', (req, res) => {
+  res.json({message: 'hooray! welcome to our api!'})
+})
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router)
+
+// =============================================================================
+app.listen(port)
+console.log('Magic happens on port ' + port)
